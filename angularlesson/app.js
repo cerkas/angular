422cced55aab533b61b6a4e2f8d46bcb69d333d0
WORK\can <cerkas1993@gmail.com>
2017-04-19 03:20:23 +0300

    задачник на Angular

---
+++ angularlesson/app.js
@@ -0,0 +1,21 @@
+ angular.module("myApp",[])
+  .controller("firstCtrl", function($scope){
+  	// Create ariable
+  	$scope.tempInput="Привет";
+  	//Array
+  	$scope.tasksArray=["Привет", "пИДР"];
+    //Function
+    $scope.addNew =function(){
+    	if ($scope.tempInput) {
+    	$scope.tasksArray.push($scope.tempInput);
+    	$scope.tempInput="";
+    }else{
+    	alert("Пусто")
+    }
+    }
+    $scope.deleteItem =function(item){
+      var index= $scope.tasksArray.indexOf(item)
+      console.log(index);
+      $scope.tasksArray.splice(index,1);
+    }
+  });
